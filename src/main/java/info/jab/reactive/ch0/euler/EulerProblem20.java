0c0d84659fa35c3e7d35f5caaa8f11b8a8a9b243
alejandro.aranguez <alejandro.aranguez@vass.es>
2021-06-01 15:40:35 +0200

    challenge EulerProblem20

--- src/main/java/info/jab/reactive/ch0/euler/EulerProblem20.java
+++ src/main/java/info/jab/reactive/ch0/euler/EulerProblem20.java
@@ -1,6 +1,10 @@
 package info.jab.reactive.ch0.euler;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.math.BigDecimal;
+import java.util.Comparator;
 
 /**
  * Problem 20: Factorial digit sum
@@ -17,8 +21,20 @@ import reactor.core.publisher.Mono;
 public class EulerProblem20 {
 
     public Mono<Long> ReactorSolution(Long limit) {
+        Mono<Long> response = Flux.range(1, limit.intValue())//create a range from 1 to limit
+                .sort(Comparator.reverseOrder()) //reverse order stream
+                .map(BigDecimal::new) //convert int to BigDecimal value
+                .reduce((x,y) -> x.multiply(y))//multiply reversed stream values
+                .map(result -> this.getSum(result)); //map value and apply getSum function
 
-        return null;
+        return response;
+    }
+
+    /**
+     * Given a BigDecimal, sum his digits and return Long value
+     */
+    private Long getSum(BigDecimal num) {
+       return Long.valueOf(num.toString().chars().map(Character::getNumericValue).sum());
     }
 
 }
